e8185f0a0a972b360ff5c9cc89c19c7f849a9e6e
branislavs369 <branislav369@gmail.com>
2019-10-04 17:01:59 +0200

    sdf

--- djangogirls/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\nay sl\desktop\django projekt\djangogirls\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
